b6714e60f25f640264a85438a9225f649b5dd789
jaanu22031 <jahnavi22031@gmail.com>
2025-05-22 22:52:55 +0530

    changes done in main.tsx

--- portfolio-vite/src/main.tsx
+++ portfolio-vite/src/main.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App.jsx'; // Import your main App component from the nested src
-import './src/index.css'; // Import your global styles from the nested src
+import App from './src/App.jsx';
+import './src/index.css';
 
-// Ensure you have a div with id='root' in your index.html
-const rootElement = document.getElementById('root') as HTMLElement; // Explicitly cast to HTMLElement
+const rootElement = document.getElementById('root') as HTMLElement;
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    <React.StrictMode> {/* Use as a component */}
+    <React.StrictMode>
       <App />
     </React.StrictMode>
   );
